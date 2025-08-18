be999dcae1bbe20262154e9308ae219a2978bb01
carlchen <carlchen>
2025-08-18 16:49:05 +0800

    test: 测试

--- src/test.js
+++ src/test.js
@@ -1,1 +1,16 @@
-console.log(1+3);
+if(1+3 ==4){
+  console.log(4);
+}
+
+const a = 1
+const b = 2
+const c = a + b
+console.log(c);
+
+const d = a - b
+console.log(d);
+
+const e = a * b
+console.log(e);
+
+
